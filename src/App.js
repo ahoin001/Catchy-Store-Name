5b27bc5ee5f46174eae3f86ed1ea06e31338da16
Alexander Hoinville <ahoin001@gmail.com>
2020-04-29 19:27:34 -0400

    Add react router and do some initial setup with it

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@ import React from 'react';
 import HomePage from "./pages/homepage/HomePage";
 import './App.css'
 
+import { Switch, Route } from "react-router-dom";
+import X from './pages/detail/x';
+
 const App = () => {
   return (
+
     <div className="App">
-      <HomePage/>
+
+
+
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path="/about/:aParam">
+          <X />
+        </Route>
+      </Switch>
+
     </div>
+
+
   );
 };
 
