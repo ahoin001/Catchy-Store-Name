20a13cc98b3f0c3de8ea6f87a59d53a6c6bf8d47
Alexander Hoinville <ahoin001@gmail.com>
2020-05-05 18:28:22 -0400

    Added redux and created firts reducer

--- src/index.js
+++ src/index.js
@@ -3,14 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// ? Redux provider so whole app has access to store and reducers
+import { Provider } from 'react-redux'
+
 import { BrowserRouter as Router, } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
 
-    <Router>
-      <App />
-    </Router>
+    <Provider>
+
+      <Router>
+
+        <App />
+
+      </Router>
+
+    </Provider>
+
 
   </React.StrictMode>,
   document.getElementById('root')
