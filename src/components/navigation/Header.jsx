f4e3e86fc22b139bbea7272552c5aa6332033a00
Alexander Hoinville <ahoin001@gmail.com>
2020-05-08 20:05:33 -0400

    Add cartdropdown reducer to redux store to be able to toggle it from different components, aallowing state in header to be changed for displaying it or keeping it hidden

--- src/components/navigation/Header.jsx
+++ src/components/navigation/Header.jsx
@@ -2,16 +2,15 @@ import React from 'react';
 import { Link, withRouter } from "react-router-dom";
 // HOF that allows component to access redux store and dispatch actions
 import { connect } from "react-redux";
-
 import { auth } from '../config/firebase/firebase-util'
 
 // ? Necesssary naming to import svg file
 import { ReactComponent as Logo } from '../../assets/logo.svg'
-
 import CartIcon from '../cart-icon/cart-icon'
-
+import CartDropdown from '../cart-dropdown/cart-dropdown';
 
 import './header-styles.scss'
+import cartDropdownReducer from '../../redux/cart-dropdown/cart-dropdown-reducer';
 
 const Header = (props) => {
 
@@ -60,9 +59,16 @@ const Header = (props) => {
 
                 }
 
-                <CartIcon/>
+                <CartIcon />
 
             </div>
+
+            {
+                props.cartDropdownHidden ?
+                    null :
+                    <CartDropdown />
+            }
+
 
         </div>
     );
@@ -74,7 +80,8 @@ const mapStateToProps = (state) => (
 
     // ? This object will be spread to props object and available for connected connected component
     {
-        currentUser: state.user.currentUser
+        currentUser: state.user.currentUser,
+        cartDropdownHidden: state.cartDropDown.hideCart
     }
 
 )
--- src/components/shared/button/custom-button.jsx
+++ src/components/shared/button/custom-button.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import './custom-button.scss'
+
 const CustomButton = (props) => {
-   
+   console.log(`Button props: `,props)
     return (
         
         <button className={`${props.isGoogleBtn ? 'google-btn' : ''} custom-bttn`}>
