a78d91000bd15119475f09cdffd928d157282ee2
Alexander Hoinville <ahoin001@gmail.com>
2020-05-30 22:24:20 -0400

    Converted async thunk to Saga in shop redux

--- src/redux/shop/shop-actions.js
+++ src/redux/shop/shop-actions.js
@@ -1,5 +1,4 @@
 import { ShopActionTypes } from './shop-types'
-import { firestore, convertCollectionSnapShotToMap } from '../../components/config/firebase/firebase-util'
 
 // ? Switches Reducer fetching state
 export const fetchCollectionsStart = () => {
@@ -23,24 +22,3 @@ export const fetchCollectionsFailure = (errorMessage) => ({
     payload: errorMessage
 
 })
-
-// ? With thunk middleware enabled, any dispatch of a function instead of an object will 
-// ? be given dispatch as it's' first argument. Now can dispatch multiple actions so we can do multiple async actions
-export const fetchCollectionsStartAsync = () => {
-
-    // ? Thunks are just functions that return a function with access to dispatch, so we can dispatch multiple actions
-    return dispatch => {
-
-        const collectionRef = firestore.collection('collections')
-        dispatch(fetchCollectionsStart())
-
-        collectionRef.get()
-            .then(snapshot => {
-                const collectionsMap = convertCollectionSnapShotToMap(snapshot)
-                // console.log(`&&&&&&&&&&&&&&&&&&&&&&&&& MAP: `,collectionsMap)
-                dispatch(fetchCollectionsSuccess(collectionsMap))
-            })
-            .catch(error => dispatch(fetchCollectionsFailure(error)))
-
-    }
-}
