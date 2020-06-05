b7645537838a0f9becc7317b76a7fc16fc95f867
Alexander Hoinville <ahoin001@gmail.com>
2020-06-04 23:24:43 -0400

    Created helper function to prevent repeated code in sign in logic for google and email

--- src/redux/user/user-sagas.js
+++ src/redux/user/user-sagas.js
@@ -9,6 +9,14 @@ import {
     signInFailure
 } from './user-actions'
 
+export function* onEmailSignInStart() {
+    yield takeLatest(UserActionTypes.EMAIL_SIGN_IN_START, signInWithEmail)
+}
+
+export function* onGoogleSignInStart() {
+    yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle)
+}
+
 export function* signInWithGoogle() {
 
     try {
@@ -17,26 +25,12 @@ export function* signInWithGoogle() {
         const { user } = yield auth.signInWithPopup(googleProvider);
         console.log(`###### `, user)
 
-        const userRef = yield call(createUserProfileDocument, user);
-        const userSnapShot = yield userRef.get();
-        console.log(`Snapshot :`, userSnapShot)
-        const l = { ...userSnapShot.data() }
-        console.log(l)
-        // ? Use user Data to create action with user Object as payload
-        // ? put sends return object back to redux flow
-        yield put(signInSuccess({
-            id: userSnapShot.id,
-            ...userSnapShot.data()
-        }))
+        yield getSnapshotFromUserAuth(user)
 
     } catch (error) {
         yield put(signInFailure(error))
     }
 
-}
-
-export function* onGoogleSignInStart() {
-    yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle)
 }
 
 export function* signInWithEmail({ payload: { email, password } }) {
@@ -45,25 +39,13 @@ export function* signInWithEmail({ payload: { email, password } }) {
 
         // ? Extract user auth object from auth sign in 
         const { user } = yield auth.signInWithEmailAndPassword(email, password);
-        const userRef = yield call(createUserProfileDocument, user);
-        const userSnapShot = yield userRef.get();
-
-        // ? Use user Data to create action with user Object as payload
-        // ? put sends return object back to redux flow
-        yield put(signInSuccess({
-            id: userSnapShot.id,
-            ...userSnapShot.data()
-        }))
+        yield getSnapshotFromUserAuth(user)
 
 
     } catch (error) {
         yield put(signInFailure(error))
     }
 
-}
-
-export function* onEmailSignInStart() {
-    yield takeLatest(UserActionTypes.EMAIL_SIGN_IN_START, signInWithEmail)
 }
 
 // ? Export Sagas to root rootSaga
@@ -73,5 +55,25 @@ export function* userSagas() {
         call(onGoogleSignInStart),
         call(onEmailSignInStart)
     ])
+
+}
+
+export function* getSnapshotFromUserAuth(userAuth) {
+
+    try {
+
+        const userRef = yield call(createUserProfileDocument, userAuth);
+        const userSnapShot = yield userRef.get();
+
+        // ? Use user Data to create action with user Object as payload
+        // ? put sends return object back to redux flow
+        yield put(signInSuccess({
+            id: userSnapShot.id,
+            ...userSnapShot.data()
+        }))
+
+    } catch (error) {
+        yield put(signInFailure(error))
+    }
 
 }
